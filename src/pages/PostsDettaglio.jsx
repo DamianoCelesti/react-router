aeb2d67f1a5d81009213798916106c88af4c8cbf
DamianoCelesti <damiano.celesti@gmail.com>
2025-02-25 20:50:26 +0100

    fix postdettaglio

--- src/pages/PostsDettaglio.jsx
+++ src/pages/PostsDettaglio.jsx
@@ -1,5 +1,25 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
 export default function PostsDettaglio() {
+
+    const { id } = useParams();
+
+    const [post, setPost] = useState({});
+
+    function fetchPost() {
+        axios.get(`http://localhost:3000/posts/${id}`)
+            .then(res => setPost(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => fetchPost(),
+        [])
+
+
+
     return (
-        <h2>Dettaglio</h2>
+        <h2>Dettaglio {post.title}</h2>
     )
 }
